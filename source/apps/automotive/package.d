216d9497d91e3053b6bfdfa2f300382e00c30bdf
UIMSolutions <ui.manufaktur@gmail.com>
2023-05-17 11:32:58 +0200

    Update

--- source/apps/automotive/package.d
+++ source/apps/automotive/package.d
@@ -2,13 +2,33 @@ module apps.automotive;
 
 mixin(ImportPhobos!());
 
-public { // required uim libraries
-  import uim.core;
-  import uim.oop;
-  import uim.apps;
+// Dub
+public import vibe.d;
+
+// UIM
+public import uim.core;
+public import uim.bootstrap;
+public import uim.html;
+public import uim.oop;
+public import uim.models;
+public import uim.apps;
+public import uim.controls;
+public import uim.servers;
+
+public import langs.javascript;
+
+public {
+  import apps.automotive.controllers;
+  import apps.automotive.helpers;
+  import apps.automotive.tests;
+  import apps.automotive.views;
 }
 
-public { // admin-automotive libraries
-  import apps.automotive.controllers;
-  import apps.automotive.views;
+DApp autApp;
+static this() {
+  automotiveApp = App
+    .name("automotiveApp")
+    .rootPath("/apps/automotive")
+    .addRoute(Route("", HTTPMethod.GET, ATMIndexPageController));
+    .addRoute(Route("/", HTTPMethod.GET, ATMIndexPageController));
 }
