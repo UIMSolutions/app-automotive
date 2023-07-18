da1df3fb120f890a5cb492e964721ab3c49a0db1
UIMSolutions <ui.manufaktur@gmail.com>
2023-07-18 14:06:09 +0200

    Update

--- source/apps/automotive/package.d
+++ source/apps/automotive/package.d
@@ -32,7 +32,7 @@ public {
 
 static this() {
   AppRegistry.register("apps.automotive",   
-    App("automotiveApp", "/apps/automotive")
+    App("automotiveApp", "apps/automotive")
       .importTranslations()
       .addRoutes(
         Route("", HTTPMethod.GET, IndexPageController),
